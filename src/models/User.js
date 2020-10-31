a9bc1c135b6f0aebe175014b506891c560c0720d
musekwa <musekwa2011@gmail.com>
2020-10-31 14:53:40 +0200

    user-registration-api

---
+++ src/models/User.js
@@ -0,0 +1,73 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require(jsonwebtoken);
+
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowecase: true,
+        validate: value =>{
+            if(!validator.isEmail(value)){
+                throw new Error({error: "Invalid Email Address"})
+            }
+        }
+    },
+
+    password: {
+        type: String,
+        required: true,
+        minLength: 7
+    },
+
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
+})
+
+userSchema.pre("save", async function(next){
+    //has the password before saving the user model
+    const user = this;
+    if (user.isModified("password")){
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+    next();
+})
+
+
+userSchema.methods.generateAuthToken = async function(){
+    //generate an auth token for the user
+    const user = this
+    const token = jwt.sign({_id: user._id}, process.env.JWT_KEY)
+    user.tokens = user.tokens.concat({token})
+    await user.save();
+    return token;
+}
+
+userSchema.statics.findByCredentials = async (email, password)=>{
+    //search for a user by email and password
+    const user = await user.findOne({email})
+    if (!user){
+        throw new Error({error: "Invalid login credentials"})
+    }
+    const isPasswordMatch = await bcrypt.compare(password, user.password)
+    if (!isPasswordMatch){
+        throw new Error({error: "Invalid login credentials"})
+    }
+    return user
+}
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
---
+++ src/routers/user.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const User = require("../models/User");
+
+const router = express.Router();
+
+router.post("/users", async (req, res)=>{
+    //create a new user
+    try {
+        const user = new User(req.body)
+        await user.save();
+        const token = await user.generateAuthToken()
+        res.status(201).send({user, token})
+    }
+    catch(error){
+        res.status(400).send(error)
+    }
+})
+
+router.post("/user/login", async (req, res)=>{
+    //login a registered user
+    try {
+        const {email, password } = req.body;
+        const user = await User.findByCredentials(email, password);
+        if(!user){
+            return res.status(401).send({error: "Login failed! Check authentication credentials"});
+        }
+        const token = await user.generateAuthToken();
+        res.send({user, token}) 
+    }
+    catch(error){
+        res.status(400).send(error)
+    }
+})
+
+
+module.exports = router
